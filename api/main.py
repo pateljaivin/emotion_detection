43c24f450d99a823ffc5d9af8b068872a42b772f
jaivin <jaivinpatel1807@gmail.com>
2023-11-04 17:34:08 +0530

    demo changes for deploy (comment all code 17:15)

--- api/main.py
+++ api/main.py
@@ -3,8 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return "Hello, World!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route("/about")
+def home():
+    return "hello"
